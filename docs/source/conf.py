66beb4cb913b8a63247c42e993566dff549a0fa8
giuliabelotti <giulia.belotti@gmail.com>
2022-10-16 02:23:11 +0200

    conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,11 +24,9 @@ extensions = ['sphinx.ext.autodoc']
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'italiano'
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_static_path = ['_static']
+html_static_path = []
 
