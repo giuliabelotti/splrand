e10c85b2e4b43aa73c5db10aa4e4fbf293134d22
giuliabelotti <giulia.belotti@gmail.com>
2022-10-16 02:19:11 +0200

    conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,5 @@ language = 'italiano'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_static_path = []
+html_static_path = ['_static']
 
-master_doc = 'index'
